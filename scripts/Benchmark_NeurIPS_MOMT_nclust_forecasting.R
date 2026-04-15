e9098dd6dafcdeedaa7e5908cb1b4c2be2881eab
Alexia Grenouillat <98542975+Grenouil@users.noreply.github.com>
2026-04-15 17:08:00 +0200

    Update Benchmark_NeurIPS_MOMT_nclust_forecasting.R

--- scripts/Benchmark_NeurIPS_MOMT_nclust_forecasting.R
+++ scripts/Benchmark_NeurIPS_MOMT_nclust_forecasting.R
@@ -263,7 +263,7 @@ tryCatch({
           )
 
           ll_val <- tryCatch({
-            logL_GP_outside_package(
+            MagmaClustR:::logL_GP(
               hp = hp_tmp, db = sub_data_agg_format_logL,
               mean = mean_vec, kern = convolution_kernel, post_cov = 0,
               pen_diag = 1e-10,
