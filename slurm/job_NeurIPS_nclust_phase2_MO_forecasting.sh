5e17f2e193d04d37cb72bcfd5d61a49a6720afe4
Alexia Grenouillat <98542975+Grenouil@users.noreply.github.com>
2026-04-18 10:39:11 +0200

    Update job_NeurIPS_nclust_phase2_MO_forecasting.sh

--- slurm/job_NeurIPS_nclust_phase2_MO_forecasting.sh
+++ slurm/job_NeurIPS_nclust_phase2_MO_forecasting.sh
@@ -10,14 +10,14 @@
 
 #SBATCH --job-name=neurips_nclust_mo_fc
 #SBATCH --qos=huge
-#SBATCH -c 32
+#SBATCH -c 25
 #SBATCH --time=7-00:00:00
 #SBATCH --output=/scratch/%u/logs/neurips_nclust_mo_fc_%j.out
 #SBATCH --error=/scratch/%u/logs/neurips_nclust_mo_fc_%j.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=alexia.grenouillat@math.univ-toulouse.fr
 
-N_WORKERS=32
+N_WORKERS=25
 N_SEEDS=50
 
 echo "=============================================="
